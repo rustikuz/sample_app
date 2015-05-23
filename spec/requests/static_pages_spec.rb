722f1da55f87757946fd667c90508d89f0ad6f35
Rustam Bazarov <rustam.bazarov@gmail.com>
2015-05-23 10:32:45 +0500

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,15 +5,15 @@ describe "StaticPages" do
   describe "Home page" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_home_path
+      get root_path
       response.status.should be(200)
     end
   end
 
-describe "Help" do
+describe "Help page" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_about_path
+      get help_path
       response.status.should be(200)
     end
   end
@@ -21,9 +21,19 @@ describe "Help" do
 describe "About page" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get static_pages_help_path
+      get about_path
       response.status.should be(200)
     end
   end
+
+describe "Contact page" do
+    it "works!" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      get contact_path
+      response.status.should be(200)
+#	  expect(response.body).to include("Ruby on Rails Tutorial Sample App | Contact.")
+    end
+  end
+
 
 end
